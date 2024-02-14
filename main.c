eb345bc75a1021f7a09180c285bca2b0a8d4146d
nicolasque <nicolasque1@gmail.com>
2024-02-14 21:08:33 +0100

    Remove unused code and update main function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 21:33:08 by nquecedo          #+#    #+#             */
-/*   Updated: 2024/02/14 14:03:42 by nquecedo         ###   ########.fr       */
+/*   Updated: 2024/02/14 21:08:04 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,46 +62,72 @@ char	*ft_strdup(char *str)
 	return (new_str);
 }
 
-char	*find_path(char **envp)
-{
-	while (ft_strncmp("PATH", *envp, 4))
-		envp++;
-	return (*envp + 5);
-}
+// char	*find_path(char **envp)
+// {
+// 	while (ft_strncmp("PATH", *envp, 4))
+// 		envp++;
+// 	return (*envp + 5);
+// }
 
 int get_infile_fd(char **argv)
 {
 	int verify_fd;
 
-	verify_fd = open(argv[1], O_RDONLY);
+	printf("infile: %s\n", argv[1]);
+	verify_fd = open(argv[1], O_RDONLY, 0777);
 	if (verify_fd == -1)
-		return (printf("infile no valido "),-1);	
+		return (printf("infile no valido \n"),-1);	
 	return (verify_fd);
 }
 
-int	get_outfile_fd(int argc, char **argv)
+int	get_outfile_fd(int argc, char *argv[])
 {
 	int verify_fd;
 
+	printf("outfile: %s\n", argv[argc - 1]);
 	verify_fd = open(argv[argc - 1],O_WRONLY | O_CREAT | O_APPEND, 0777);
 	if (verify_fd == -1)
-		return (printf("infile no valido "),-1);
+		return (printf("infile no valido \n"),-1);
 	return (verify_fd);
 }
 
-int main(int argc, char **argv, char **envp) 
+int main(int argc, char **argv) 
 {
-	int fd[2];
+	int pipe_fd[2];
+	int fd_in;
+	int fd_out;
+	int pid1;
+	int pid2;
 	
 	// 0 infile, 1 outfile
-	fd[0] = get_infile_fd(argv);
-	fd[1] = get_outfile_fd(argc, argv);
-	if (fd[0] == -1 || fd[1] == -1)
+	fd_in = get_infile_fd(argv);
+	fd_out = get_outfile_fd(argc, argv);
+	if (fd_in == -1 || fd_out == -1)
 		return (perror("algo malo con los fd"), -1);
-	printf("Infile: %i\n" ,fd[0]);
-	printf("outfile: %i", fd[1]);
 	
-	close(fd[0]);
-	close(fd[1]);
+	if (pipe(pipe_fd) == -1)
+		return (perror("Problema con el pipe"), 1);	
+
+	pid1 = fork();
+	if (pid1 < 0)
+		return (perror("Porblem wit fork 1"), 2);
+	
+	if (pid1 == 0)
+	{//Child proces
+		dup2(fd_in, 0);
+		dup2(pipe_fd[1], 1);
+		close(pipe_fd[0]);
+		execlp(argv[2], argv[2], "-l", NULL);
+		exit(1);
+	}
+	// Parent proces
+		dup2(fd_out, 1);
+		dup2(pipe_fd[0],0);
+		close(pipe_fd[1]);
+		execlp(argv[3], argv[3], "-l", NULL);
+		exit(1);
+
+
+	return (0);
 }
 
